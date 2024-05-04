638337b320a689108cf97a55ca76ce972d1b6288
NOOR <75979709+nooriq1@users.noreply.github.com>
2024-05-04 13:05:15 +0300

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php 
+//Start Session
+session_start();
+
+//Create Constants to save Database Credentials
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'id22121864_nooriqtask_manager');
+define('DB_PASSWORD', 'Aa112233Ss@');
+define('DB_NAME', 'id22121864_task_manager');
+
+define('SITEURL', 'http://localhost/task-manager/');
+
+?>
